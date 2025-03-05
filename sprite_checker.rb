612c5f68c4d18b4f3d5415143ec4f189756d7f5d
gabriel <gabrielaguirre921@gmail.com>
2025-03-05 11:24:17 +0100

    update script for sprites

--- sprite_checker.rb
+++ sprite_checker.rb
@@ -1,64 +1,72 @@
 require 'json'
 require_relative 'env'
 
-puts "Opening PvPoke Game Master..."
-file = File.open("v1/gamemaster.json")
+white_list = [
+  29, # Nidoran Female
+  32, # Nidoran Male
+  122, # Nidoran Male
+  250, # Ho-Oh
+  474, # Porygon-Z
+  439, # Mime (Jr)
+  782, # Jangmo-o
+  783, # Hakamo-o
+  784, # Kommo-o
+  785, # Tapu Koko
+  786, # Tapu Lele
+  787, # Tapu Bulu
+  788, # Tapu Fini
+  866, # MR. Rime
+  1001, # Wo Chien
+  1002, # Chien Pao
+  1003, # Ting Lu
+  1004 # Chi Yu
+]
+puts 'Opening PvPoke Game Master...'
+file = File.open('v1/gamemaster.json')
 
-puts "Fetching Contents"
+puts 'Fetching Contents'
 content = file.read
 
-puts "Parsing JSON"
+puts 'Parsing JSON'
 json = JSON.parse(content)
 
 puts 'Checking sprites...'
 required = []
 optional = []
-json['pokemon'].each { |p| 
-    dex = p['dex']
-    id = p['speciesId']
 
-    if id.include?("mega") 
-        next
-    end
+json['pokemon'].each do |p|
+  dex = p['dex']
+  id = p['speciesId']
 
-    speciesId = id.gsub("_xl", "").gsub("_xs", "").gsub("_shadow", "")
+  species_id = id.gsub('_xl', '').gsub('_xs', '').gsub('_shadow', '')
 
-    if speciesId == "mr_mime_galarian"
-        name = speciesId.split('_');
-        spriteName = "#{dex}-#{name[2]}";
-    elsif (speciesId.include?("_") &&
-            dex != 29 && # Nidoran Female
-            dex != 32 && # Nidoran Male
-            dex != 122 && # Nidoran Male
-            dex != 250 && # Ho-Oh
-            dex != 474 && # Porygon-Z
-            dex != 439 && # Mime (Jr)
-            dex != 782 && # Jangmo-o
-            dex != 783 && # Hakamo-o
-            dex != 784 && # Kommo-o
-            dex != 785 && # Tapu Koko
-            dex != 786 && # Tapu Lele
-            dex != 787 && # Tapu Bulu
-            dex != 788 && # Tapu Fini
-            dex != 866) # MR. Rime
-        spriteName = "#{dex}-#{id.split('_')[1]}";
+  if species_id.include?('_alolan')
+    sprite_name = "#{dex}-alolan"
+  elsif species_id.include?('_galarian')
+    sprite_name = "#{dex}-galarian"
+  elsif species_id.include?('_hisuian')
+    sprite_name = "#{dex}-hisuian"
+  elsif species_id.include?('_') && !white_list.include?(dex)
+    names = species_id.split('_')
+    names[0] = dex
+    sprite_name = names.join('-')
+  else
+    sprite_name = dex.to_s
+  end
+
+  unless File.file?("#{Env::SPRITES_PATH}#{sprite_name}.png")
+    if p['released']
+      required.push("name: #{id} sprite: #{sprite_name}")
     else
-        spriteName = "#{dex}";
+      optional.push("name: #{id} sprite: #{sprite_name}")
     end
-
-    unless (File.file?("#{Env::SPRITES_PATH}#{spriteName}.png"))
-        if (p['released']) 
-            required.push("name: #{id} sprite: #{spriteName}")
-        else
-            optional.push("name: #{id} sprite: #{spriteName}")
-        end
-    end
-}
+  end
+end
 
 puts 'Required:'
 puts
 required.each { |name| puts "\e[31m#{name}\e[0m" }
-puts 
+puts
 
 puts 'Optional:'
 puts
